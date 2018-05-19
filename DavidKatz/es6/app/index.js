8875f63a2c2096f958dd22e6bc53745c7a8ff6ee
Colombianito66 <colombianito66@gmail.com>
2018-05-19 12:58:53 -0400

    filter method practice

--- DavidKatz/es6/app/index.js
+++ DavidKatz/es6/app/index.js
@@ -1,3 +1,13 @@
+const averages = [6, 15.7, 9, 18, 22.1, 9.2, 4];
+
+const celebration = function(player) {
+  return player > 15;
+};
+
+const highlight = average.filter(celebration);
+
+console.log('---------');
+
 let isPassing = grade => {
   return grade >= 70;
 };
