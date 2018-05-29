66caafb6c398e3d85dd63f3b3e1585ae89129a24
Colombianito66 <colombianito66@gmail.com>
2018-05-29 14:16:46 -0400

    practice props / lists

--- DavidKatz/countdownchamp/src/App.js
+++ DavidKatz/countdownchamp/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Series from './Series.js';
 import 'whatwg-fetch';
+
 
 const Intro = (props) => (
   <p className='App-Intro'>
@@ -8,36 +10,12 @@ const Intro = (props) => (
 )
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch(`http://api.tvmaze.com/search/shows?q=Vikings`)
-      .then((response) => response.json())
-      .then(json => {
-        this.setState({
-          series: this.state.series.concat(json)
-        })
-      });
-
-      // Example Using SetTimeout to update State
-      const shows = ['Garfield', 'Happy Days'];
-      setTimeout(() => {
-        this.setState({ series: this.state.series.concat(shows) });
-      }, 3000);
-      
-      
-    }
-    
     getCurrentDate() {
       let date = new Date();
       return date.toDateString();
     }
     
     render() {
-      
-      console.log(this.state.series)
     return (
       <div> 
         <h1>Countdown - Stevie - App Component </h1>
@@ -45,7 +23,8 @@ class App extends Component {
         <hr/>
         <h1>TV SERIES LIST </h1>
         <Intro message='Here you can find all your love:)' />
-        Series Array Length - {this.state.series.length}
+        
+        <Series />
       </div>
 
     )
