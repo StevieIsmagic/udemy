a41b2adbd336f36570f55d9eee69b7514d81f360
Colombianito66 <colombianito66@gmail.com>
2018-06-26 03:15:45 -0400

    refactor scraper to utils dir

--- Turbo360/learning-links/routes/sms.js
+++ Turbo360/learning-links/routes/sms.js
@@ -2,6 +2,9 @@
 const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID })
 const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
 const router = vertex.router()
+const superagent = require('superagent')
+const scrape = require('../utils/scraper')
+
 
 router.get('/', (req, res) => {
   res.json({
@@ -30,6 +33,39 @@ router.post('/', (req, res) => {
     })
     return
   }
+  // checks if response message is an http URL link
+  if (message.indexOf('http') != -1) {
+    superagent
+      .get(url)
+      .query(message)
+      .end((err, response) => {
+        if (err) {
+          res.json({
+            confirmation: 'fail',
+            message: err.message
+          })
+          return
+        }
+
+        const html = response.text
+        var tags = scrape.scraper(html)
+
+        turbo.create('link', tags)
+          .then(data => {
+              res.json({
+                confirmation: 'success',
+                data: data
+              })
+          })
+          .catch(err => {
+            res.json({
+              confirmation: 'fail',
+              message: err.message
+            })
+          })
+      })
+    return
+  }
 
   var sms = {
     from: from,
@@ -48,9 +84,8 @@ router.post('/', (req, res) => {
       res.json({
         confirmation: 'fail',
         message: err.message
-      }) 
+      })
     })
-
 })
 
 
--- Turbo360/learning-links/routes/tags.js
+++ Turbo360/learning-links/routes/tags.js
@@ -2,7 +2,7 @@ const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID })
 const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
 const router = vertex.router()
 const superagent = require('superagent')
-const cheerio = require('cheerio')
+const scrape = require('../utils/scraper')
 
 router.get('/', (req, res) => {
   var url = req.query.url
@@ -15,44 +15,7 @@ router.get('/', (req, res) => {
     return
   }
 
-  superagent
-    .get(url)
-    .query(null)
-    .end((err, response) => {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err.message
-        })
-        return
-      }
-
-      var tags = {}
-      const html = response.text
-      $ = cheerio.load(html)
-      $('meta').each(function(i, meta){
-        const attribs = meta.attribs
-        if (attribs == null) return true
-
-        const property = attribs.property
-        if (property == null) return true
-    
-        if (property == 'og:title') {
-          tags['title'] = attribs.content
-        }
-        if (property == 'og:description') {
-          tags['description'] = attribs.content
-        }
-        if (property == 'og:image') {
-          tags['image'] = attribs.content
-        }   
-      })   
-      res.json({
-        confirmation: 'success',
-        tags: tags
-      })
-    
-    })
+  
 })
 
 module.exports = router
