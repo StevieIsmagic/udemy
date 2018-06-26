0f755cf95e89ad80a60f3e16c5b926426303ff71
Colombianito66 <colombianito66@gmail.com>
2018-06-25 21:57:48 -0400

    handle twilio webhook response

--- Turbo360/learning-links/routes/sms.js
+++ Turbo360/learning-links/routes/sms.js
@@ -1,10 +1,6 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require('turbo360')({
-  site_id: process.env.TURBO_APP_ID
-})
-const vertex = require('vertex360')({
-  site_id: process.env.TURBO_APP_ID
-})
+const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID })
+const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
 const router = vertex.router()
 
 router.get('/', (req, res) => {
@@ -18,8 +14,42 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   var body = req.body // data from Twilio
   var message = body['Body']
+  if (message === null) {
+    res.json({
+      confirmation: "fail",
+      message: err.message
+    })
+    return
+  }
 
   var from = body['From'] // phone #
+  if (from === null) {
+    res.json({
+      confirmation: "fail",
+      message: err.message
+    })
+    return
+  }
+
+  var sms = {
+    from: from,
+    message: message
+  }
+
+  // create document in MongoDB
+  turbo.create('sms', sms)
+    .then(data => {
+      res.json({
+        confirmation: 'success',
+        data: data
+      })
+    })
+    .catch(err => {
+      res.json({
+        confirmation: 'fail',
+        message: err.message
+      }) 
+    })
 
 })
 
