6dc54d498d048461b4c27d9876c1eed39de89002
Colombianito66 <colombianito66@gmail.com>
2018-07-06 16:47:08 -0400

    testing

--- DavidKatz/mongoForm/mongoform/server.js
+++ DavidKatz/mongoForm/mongoform/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: "Hello from Express Server" });
+  res.send({ express: "Hello from Express Server:)" });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
