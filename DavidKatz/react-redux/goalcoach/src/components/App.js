004b7255dda7514e59d0b90c7978b3b07faf0d77
Colombianito66 <colombianito66@gmail.com>
2018-08-06 23:01:12 -0400

    ad idem

--- DavidKatz/react-redux/goalcoach/src/components/App.js
+++ DavidKatz/react-redux/goalcoach/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 class App extends Component {
   render() {
     return (
       <div> App </div>
+
     )
   }
 }
